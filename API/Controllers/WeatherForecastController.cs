32229351fad814d63bb7f2391d97fec7a026371f
IBXCODECAT <nathan.schmitt7@gmail.com>
2023-09-14 15:10:21 -0500

    Add DB

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Domain;
+using Persistance;
 
 namespace API.Controllers;
 
@@ -13,10 +15,14 @@ public class WeatherForecastController : ControllerBase
 
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    private readonly DataContext _context;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, DataContext context)
     {
         _logger = logger;
+        _context = context;
     }
+
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
@@ -28,5 +34,37 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+    [HttpPost]
+    public ActionResult<WeatherForecast> Create()
+    {
+        Console.WriteLine($"Database Path: {_context.dbPath}");
+        Console.WriteLine("Insert a new WeatherForecast into the database");
+
+        WeatherForecast forcast = new WeatherForecast
+        {
+            Date = new DateOnly(),
+            TemperatureC = 75,
+            Summary = "Warm"
+        };
+
+        if(_context.WeatherForecasts == null)
+        {
+            Console.WriteLine("WeatherForecasts is null");
+        }
+        else
+        {
+            _context.WeatherForecasts.Add(forcast);
+        }
+        
+        bool success = _context.SaveChanges() > 0;
+
+        if(success)
+        {
+            return forcast;
+        }
+
+        throw new Exception("Error saving WeatherForecast");
     }
 }
