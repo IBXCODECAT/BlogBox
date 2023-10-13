f5d59f51a22b1d183d4f953308a31ec0e7a35f34
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-10-13 10:45:59 -0500

    Put Request

--- API/Controllers/PostController.cs
+++ API/Controllers/PostController.cs
@@ -1,7 +1,10 @@
 using Domain;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Persistance;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Net;
 
 namespace API.Controllers
 {
@@ -42,6 +45,7 @@ namespace API.Controllers
         }
 
         [HttpPost(Name = "Create")]
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.Created)]
         public ActionResult<Post> Create([FromBody] Post request)
         {
             Post post = new Post
@@ -58,10 +62,44 @@ namespace API.Controllers
 
             if(success)
             {
-                return Ok(post);
+                return new ObjectResult(post) { StatusCode = StatusCodes.Status201Created };
             }
 
             throw new Exception("Error creating post");
+        }
+
+        [HttpPut("{id}", Name = "Update")]
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.OK)]
+        public ActionResult<Post> Update([FromBody]Post request)
+        {
+            Post? postToUpdate = context.posts.Find(request.Id);
+
+            if(postToUpdate is null)
+            {
+                Post newPost = new Post()
+                {
+                    Id = request.Id,
+                    Title = request.Title,
+                    Body = request.Body,
+                    Date = request.Date
+                };
+
+                context.posts.Add(newPost);
+
+                bool success = context.SaveChanges() > 0;
+
+                return success ? new ObjectResult(newPost) { StatusCode = StatusCodes.Status201Created } : throw new Exception("Error creating or updating resource.");
+            }
+            else
+            {
+                postToUpdate.Title = request.Title;
+                postToUpdate.Body = request.Body;
+                postToUpdate.Date = request.Date;
+
+                bool success = context.SaveChanges() > 0;
+
+                return success ? new ObjectResult(postToUpdate) { StatusCode = StatusCodes.Status200OK } : throw new Exception("Error creating or updating resource.");
+            }
         }
     }
 
