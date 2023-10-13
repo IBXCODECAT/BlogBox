b4ecb0723c75ec0a7da851f1b24834527fa981c9
IBXCODECAT <47279783+IBXCODECAT@users.noreply.github.com>
2023-10-13 11:28:08 -0500

    Complete Assignment 6

--- API/Controllers/PostController.cs
+++ API/Controllers/PostController.cs
@@ -5,6 +5,7 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerUI;
 using System.Net;
+using Microsoft.Extensions.Options;
 
 namespace API.Controllers
 {
@@ -32,16 +33,18 @@ namespace API.Controllers
         /// <param name="id">Post Id</param>
         /// <returns>A single post matching the specified id</returns>
         [HttpGet("{id}", Name = "GetPost")]
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.NotFound)]
         public ActionResult<Post> GetPost(Guid id)
         {
             Post? post = this.context.posts.Find(id);
 
             if (post is null)
             {
-                return NotFound();
+                return new ObjectResult(null) { StatusCode = StatusCodes.Status404NotFound };
             }
 
-            return Ok(post);
+            return new ObjectResult(post) { StatusCode = StatusCodes.Status200OK};
         }
 
         [HttpPost(Name = "Create")]
@@ -60,7 +63,7 @@ namespace API.Controllers
 
             bool success = context.SaveChanges() > 0;
 
-            if(success)
+            if (success)
             {
                 return new ObjectResult(post) { StatusCode = StatusCodes.Status201Created };
             }
@@ -70,11 +73,12 @@ namespace API.Controllers
 
         [HttpPut("{id}", Name = "Update")]
         [ProducesResponseType(typeof(Post), (int)HttpStatusCode.OK)]
-        public ActionResult<Post> Update([FromBody]Post request)
+        [ProducesResponseType(typeof(Post), (int)HttpStatusCode.Created)]
+        public ActionResult<Post> Update([FromBody] Post request)
         {
             Post? postToUpdate = context.posts.Find(request.Id);
 
-            if(postToUpdate is null)
+            if (postToUpdate is null)
             {
                 Post newPost = new Post()
                 {
@@ -88,7 +92,7 @@ namespace API.Controllers
 
                 bool success = context.SaveChanges() > 0;
 
-                return success ? new ObjectResult(newPost) { StatusCode = StatusCodes.Status201Created } : throw new Exception("Error creating or updating resource.");
+                return success ? new ObjectResult(newPost) { StatusCode = StatusCodes.Status201Created } : new ObjectResult(null) { StatusCode = StatusCodes.Status500InternalServerError };
             }
             else
             {
@@ -101,7 +105,32 @@ namespace API.Controllers
                 return success ? new ObjectResult(postToUpdate) { StatusCode = StatusCodes.Status200OK } : throw new Exception("Error creating or updating resource.");
             }
         }
+
+        [HttpDelete("id", Name = "Delete")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.NoContent)]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.NotFound)]
+        public ObjectResult Delete(Guid id)
+        {
+            Post? postToDelete = context.posts.Find(id);
+
+            if (postToDelete is null)
+            {
+                return new ObjectResult(postToDelete) { StatusCode = StatusCodes.Status404NotFound };   
+            }
+
+            context.posts.Remove(postToDelete);
+
+            bool success = context.SaveChanges() > 0;
+
+            if (!success)
+            {
+                return new ObjectResult(null) {StatusCode = StatusCodes.Status500InternalServerError};
+                throw new Exception("Error deleting resource.");
+            }
+            else
+            {
+                return new ObjectResult(null) { StatusCode = StatusCodes.Status204NoContent };
+            }
+        }
     }
-
-
 }
